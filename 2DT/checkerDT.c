c6b18e67f7e2ec6165f6c4ceaa1c810bbbd6a385
Jairam Hathwar <jh3258@princeton.edu>
2022-11-14 12:59:15 -0500

    assert and true/false

--- 2DT/checkerDT.c
+++ 2DT/checkerDT.c
@@ -122,7 +122,7 @@ boolean CheckerDT_isValid(boolean bIsInitialized, Node_T oNRoot,
    /* compare counter with other value (how many nodes should be there)
       2  if statements */
    if(CheckerDT_treeCheck(oNRoot, &counter)) {
-      if (counter != ulCount) {
+      if (counter < ulCount) {
          fprintf(stderr, "Total number of directories do not match \n");
          return FALSE;
       }
