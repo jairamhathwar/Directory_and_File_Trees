38b4401df147091302704b3b761bcbe604fa6171
Jairam Hathwar <jh3258@princeton.edu>
2022-11-13 23:12:00 -0500

    2.2

--- 2DT/checkerDT.c
+++ 2DT/checkerDT.c
@@ -94,7 +94,7 @@ static boolean CheckerDT_treeCheck(Node_T oNNode) {
                fprintf(stderr, "getNumChildren claims more children than getChild returns\n");
                return FALSE;
             }
-            nodeComparison = Node_compare(oNChild, oNChildPrev);
+            nodeComparison = Path_comparePath(oNChild, oNChildPrev);
             if(nodeComparison == 0) {
                fprintf(stderr, "Duplicate path detected in tree\n");
                return FALSE;
