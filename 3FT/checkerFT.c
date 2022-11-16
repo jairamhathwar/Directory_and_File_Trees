eae7be74ced97abe3ddfe4d129ebbc7c2b7558de
Jairam Hathwar <jh3258@princeton.edu>
2022-11-16 04:35:37 -0500

    entire part3

--- 3FT/checkerFT.c
+++ 3FT/checkerFT.c
@@ -65,45 +65,47 @@ static boolean CheckerFT_treeCheck(Node_T oNNode, size_t *nodeCount) {
       /* in for loop, counter increment for size of tree compare to other value. if different, print error. else, chill */
 
       /* Recur on every child of oNNode */
-      for(ulIndex = 0; ulIndex < Node_getNumChildren(oNNode); ulIndex++)
-      {
-         Node_T oNChild = NULL;
-         Node_T oNChildPrev = NULL;
-         iStatus = Node_getChild(oNNode, ulIndex, &oNChild);
+      if (!getType(oNNode)) {
+         for(ulIndex = 0; ulIndex < Node_getNumChildren(oNNode); ulIndex++)
+         {
+            Node_T oNChild = NULL;
+            Node_T oNChildPrev = NULL;
+            iStatus = Node_getChild(oNNode, ulIndex, &oNChild);
 
-         if (iStatus == NOT_A_DIRECTORY) {
-            *nodeCount = (*nodeCount)+1;
-         }
-         else if(iStatus != SUCCESS) {
-            fprintf(stderr, "getNumChildren claims more children than getChild returns\n");
-            return FALSE;
-         }
-
-         else {
-            if (ulIndex != 0) {
-               prevStatus = Node_getChild(oNNode, ulIndex-1, &oNChildPrev);
-               if(prevStatus != SUCCESS && prevStatus != NOT_A_DIRECTORY) {
-                  fprintf(stderr, "getNumChildren claims more children than getChild returns\n");
-                  return FALSE;
-               }
-               if(prevStatus != NOT_A_DIRECTORY) {
-                  nodeComparison = Path_comparePath(Node_getPath(oNChild), Node_getPath(oNChildPrev));
-                  if(nodeComparison == 0) {
-                     fprintf(stderr, "Duplicate path detected in tree\n");
-                     return FALSE;
-                  }
-                  if(nodeComparison < 0) {
-                     fprintf(stderr, "Children not in lexicographic order\n");
-                     return FALSE;
-                  }
-               }               
+            if (iStatus == NOT_A_DIRECTORY) {
+               *nodeCount = (*nodeCount)+1;
             }
+            else if(iStatus != SUCCESS) {
+               fprintf(stderr, "getNumChildren claims more children than getChild returns\n");
+               return FALSE;
+            }
+
+            else {
+               if (ulIndex != 0) {
+                  prevStatus = Node_getChild(oNNode, ulIndex-1, &oNChildPrev);
+                  if(prevStatus != SUCCESS && prevStatus != NOT_A_DIRECTORY) {
+                     fprintf(stderr, "getNumChildren claims more children than getChild returns\n");
+                     return FALSE;
+                  }
+                  if(prevStatus != NOT_A_DIRECTORY) {
+                     nodeComparison = Path_comparePath(Node_getPath(oNChild), Node_getPath(oNChildPrev));
+                     if(nodeComparison == 0) {
+                        fprintf(stderr, "Duplicate path detected in tree\n");
+                        return FALSE;
+                     }
+                     if(nodeComparison < 0) {
+                        fprintf(stderr, "Children not in lexicographic order\n");
+                        return FALSE;
+                     }
+                  }               
+               }
             *nodeCount=(*nodeCount) + 1;
 
             /* if recurring down one subtree results in a failed check
             farther down, passes the failure back up immediately */
             if(!CheckerFT_treeCheck(oNChild, nodeCount))
                return FALSE;
+            }
          }
       }
       if (getType(oNNode)) *nodeCount = (*nodeCount)+1;
