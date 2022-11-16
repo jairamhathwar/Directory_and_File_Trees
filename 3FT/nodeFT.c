003691b8d0fd7551769ff3c16dfabe06053dfd9b
Jairam Hathwar <jh3258@princeton.edu>
2022-11-15 22:13:52 -0500

    entire part3

--- 3FT/nodeFT.c
+++ 3FT/nodeFT.c
@@ -325,11 +325,11 @@ size_t Node_free(Node_T oNNode) {
                                   ulIndex);
    }
 
-      /* recursively remove children */
-      while(DynArray_getLength(oNNode->oDChildren) != 0) {
-         ulCount += Node_free(DynArray_get(oNNode->oDChildren, 0));
-      }
-      DynArray_free(oNNode->oDChildren);
+   /* recursively remove children */
+   while(DynArray_getLength(oNNode->oDChildren) != 0) {
+      ulCount += Node_free(DynArray_get(oNNode->oDChildren, 0));
+   }
+   DynArray_free(oNNode->oDChildren);
    free(oNNode->fileContents);
    /* remove path */
    Path_free(oNNode->oPPath);
