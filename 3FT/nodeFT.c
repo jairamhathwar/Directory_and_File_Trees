44ff9c47c85612d2b4e32e2097da2e0d1702c1c2
Jairam Hathwar <jh3258@princeton.edu>
2022-11-16 12:49:10 -0500

    entire part3

--- 3FT/nodeFT.c
+++ 3FT/nodeFT.c
@@ -107,8 +107,6 @@ int Node_newFile(Path_T oPPath, Node_T oNParent, Node_T *poNResult,
    assert(oPPath != NULL);
    assert(oNParent == NULL || CheckerFT_Node_isValid(oNParent));
 
-   if(getType(oNParent)) return NOT_A_DIRECTORY;
-
    /* allocate space for a new node */
    psNew = malloc(sizeof(struct node));
    if(psNew == NULL) {
@@ -205,8 +203,6 @@ int Node_newDir(Path_T oPPath, Node_T oNParent, Node_T *poNResult) {
 
    assert(oPPath != NULL);
    assert(oNParent == NULL || CheckerFT_Node_isValid(oNParent));
-
-   if(getType(oNParent)) return NOT_A_DIRECTORY;
 
    /* allocate space for a new node */
    psNew = malloc(sizeof(struct node));
